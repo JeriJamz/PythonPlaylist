277d0e5252a42d861a5679ebc262d7da1816846a
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-01-07 08:47:13 -0600

    Add files via upload

---
+++ password.py
@@ -0,0 +1,24 @@
+import sys, time
+
+password = 'Swordfish'
+
+print("I been meaning to work on something like this. Its not finished,\n")
+print("gotta couple debugs to work through.")
+
+
+for guesstaken in range(1,4):
+    print('what is the password',1)
+    guess = input()
+    if guess != password:
+        print('that is not the password')
+    elif guess == int():# not printing this 
+        print('letter only')
+    else:
+        break
+if guess == password:
+    print('Accesss granted')
+else:
+    ('tuff')#not printing this
+    sys.exit()
+        
+        
