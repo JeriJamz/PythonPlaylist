d12ed2010dd9c68a7c7b91cf9dfc9278bc5b4220
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-04-11 23:40:32 -0500

    Add files via upload

---
+++ randomForm.py
@@ -0,0 +1,17 @@
+#this is some random forms im gone work on. Figure away for user to call it
+#other words im still working that cal book
+
+class Form:
+    def __init__(self,a,b,c,num):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.num = num
+
+    def Liner_equa(self,other):
+        if num != a * b + c:
+            print("It might not be you.")
+        
+
+
+
