22142a3df0110573384a3781adc32b9606285a3b
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-04-30 03:16:20 -0500

    Add files via upload

--- Lingo.py
+++ Lingo.py
@@ -1,239 +1,218 @@
-import time,sys
-def delay_print(z):
-    for t in z:
-        sys.stdout.write(t)
-        sys.stdout.flush()
-        time.sleep(.05)
-#this is the source code
-########################
+#this is the "Source Code", saw some random guy on YT make this now im crazy bout making my own
+#ill drop his link in Rescorces
+
 #CONSTANTS
-########################
+################################
 Digits = '0123456789'
 
-#######################
 #ERRORS
-#######################
+##################################
 
 class Error:
-    def __init__(self, pos_start, pos_end, error_name, details):
+    def __init__(self,pos_start,pos_end,error_name, details):
         self.pos_start = pos_start
         self.pos_end = pos_end
         self.error_name = error_name
         self.details = details
-    
+
     def as_string(self):
-        result = f'{self.error_name}: {self.details}'#should return the error name: details
-        result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
+        result = f'{self.error_name}: {self.details}'
+        result = f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
         Return result
 
 class IllegalCharError(Error):
-    def __init__(self, pos_start, pos_end, details):
+    def __init__(seld, pos_start, pos_end, details):
         super().__init__('Illegal Character', details)
 
-########################
-# POSITION
+#POSITION
 ########################
 
 class Position:
     def __init__(self, idx, ln, col, fn, ftxt):
-        self.idx  = idx
-        self.ln = ln 
+        self.idx = idx
+        self.ln = ln
         self.col = col
-        self.fn = fn
+        self.fn = fn,
         self.ftxt = ftxt
 
     def adv(self, current_char):
-        self.idx += 1
-        self.col += 1
+        self.idx +=1
+        self.col +=1
 
         if current_char == '\n':
-            self.ln += 1 
-            self.col = 0
-
+            self.ln +=1
+            self.col +=0
+        
         Return self
-    
+
     def copy(self):
-        Return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
+        Return Position(self.idx, self.ln, se)
+        
+    def Current_Nstruct(self):
+        if current_char != '\n':
+            Return len(current_char, start_pos)
 
-#########################
-# TOKENS
-##########################
-#* Constants for TOKENS
-# TT stands for token type
-
+#TOKENS
+#######################
+# TT means Tokens 
 
 TT_INT = 'TT_INT'#0123456789
-TT_FLOAT = 'FLOAT'#.00001
-TT_PLUS = 'PLUS'# + 
-TT_MINUS = 'MINUS'# - 
-TT_MUL = 'MUL'# * 
-TT_DIV = 'DIV'# / 
-TT_LPAREN = 'LPAREN'# ( 
-TT_RPAREN = 'RPAREN'# )
-
+TT_FLOAT = 'FLOAT' #0.12358
+TT_PLUS = 'PLUS' #+
+TT_MINUS = 'MINUS'#-
+TT_MUL = 'MUL' #*
+TT_DIV = 'DIV' # /
+TT_LPAREN = 'LPAREN'#(
+TT_RPAREN = 'RPAREN'#)
 
 class Tokens:
     def __init__(self,type_,value = None):
-        self.type = type_
+        self.type_ = type_
         self.value = value
 
     def __repr__(self):
-        if self.value: return f'{self.type}:{self.value}'#if token has a value it will print the 'type:value'
-        Return f'{self.type}'#if it doesnt have a value it will print jus the type
+        if self.value: Return f'{self.type}:{self.value}'
+        Return f'{self.type_}'
 
-############################
 #LEXER
-############################
-
-class Lexer:#this is what read the text
-    def __init__(self,fn, text):
+#############################
+class Lexer:
+    def __init__(self,fn,text):
         self.fn = fn
-        self.text = text #this makes text and obj of lexer
-        self.pos = Position(-1, 0, -1, fn, text)#make sure it doesnt start at a blank space and the code starts to freak out
-        self.current_char = None#agains sets everything basically to 0 or -1
+        self.text = text
+        self.pos = Position(-1,0,-1,fn,text)
+        self.current_char = None
         self.adv()
 
     def adv(self):
-        self.pos.adv(self.current_char)
-        #below is where the lexer will advance but what stops it from being an infinite loop is the else None
-        # current char is linked with adv. and is equal to the pos of the text. 
-        self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None 
+        self.pos_adv(self.current_char)
+        self.current_char = self.text[eslf.pos.idx]
 
     def make_tokens(self):
         tokens = []
 
         while self.current_char != None:
-            if self.current_char in '\n':#this will check for blank spaces and move on
+            if self.current_char in'\n':
                 self.adv()
-            elif self.current_char in Digits:
+            elif self.currrent_char in Digits:
                 tokens.append(self.make_number())
             elif self.current_char == '+':
-                tokens.append(Tokens(TT_PLUS))#i might have to call it tokens(remember to add the S)
+                tokens.append(Tokens(TT_PLUS))
                 self.adv()
             elif self.current_char == '-':
-                tokens.append(Tokens(TT_MINUS))
+                tokens.append(tokens(TT_MINUS))
                 self.adv()
             elif self.current_char == '*':
-                tokens.append(Tokens(TT_MUL))
+                tokens.append(tokens(TT_MUL))
                 self.adv()
             elif self.current_char == '/':
-                tokens.append(Tokens(TT_DIV))
+                tokens.append(tokens(TT_DIV))
                 self.adv()
             elif self.current_char == '(':
-                tokens.append(Tokens(TT_LPAREN))
+                tokens.append(tokens(TT_LPAREN))
                 self.adv()
             elif self.current_char == ')':
-                tokens.append(Tokens(TT_RPAREN))
+                tokens.append(tokens(TT_RPAREN))
                 self.adv()
-            else:# the error
+            else:
                 pos_start = self.pos.copy()
                 char = self.current_char
                 self.adv()
-                Return [], IllegalCharError(pos_start, self.pos,"' " + char +" '")
-
+                Return [], IllegalCharError(pos_start, self.pos,"' " + char + " '")
         Return tokens, None
 
+        def make_number(self):
+            num_str = ''
+            dot_count = 0
 
-    def make_number(self):
-        num_str = ''
-        dot_count = 0
+            while self.current_char != None and self.current_char in Digits + '.':
+                if self.current_char == '.':
+                    if dot_count == 1: break
+                    dot_count += 1
+                    num_str += '.'
+                else:
+                    num_str == self.current_char
+                    self.adv()
 
-        while self.current_char != None and self.current_char in Digits + '.':
-            if self.current_char == '.':
-                if dot_count == 1: break 
-                dot_count += 1
-                num_str += '.'
+            if dot_count == 0:
+                Return tokens(TT_INT, int(num_str))
             else:
-                num_str += self.current_char
-                self.adv()
-            
-        if dot_count == 0:
-            return Tokens(TT_INT, int(num_str))
-        else:
-            return Tokens(TT_FLOAT, float(num_str))
+                Return tokens(TT_FLOAT, float(num_str))
 
-###########################
 #NUMBER NODE
-############################
-
+##################################
 class NumberNode:
     def __init__(self, tok):
         self.tok = tok
+
     def __repr__(self):
         Return f"{self.tok}"
 
 class BinOpNode:
-    def __init__(self, lft_node,op_tok, rgt_node):
+    def __init__(self, lft_node, op_tok, rgt_node):
         self.lft_node = lft_node
         self.op_tok = op_tok
         self.rgt_node = rgt_node
-    
+
     def __repr__(self):
-        Return f'({self.lft_node}, {op_tok}, {rgt_node})'
+        Return f"({self.lft_node}, {self.op_tok}, {self.rgt_node})"
 
-##########################
 #PARSER
-########################
-
+###################################
 class Parser:
     def __init__(self, tokens):
-        self.tokens = tokens 
+        self.tokens = tokens
         self.tok_idx = 1
         self.adv()
 
     def adv(self):
-        self.tok_idx += 1
+        self.tok_idx +=1
         if self.tok_idx < len(self.tokens):
             self.current_tok = self.tokens[self.tok_idx]
         Return self.current_tok
 
-#######################
-
+####################################
     def parse(self):
         res = self.expr()
         Return res
 
-
     def factor(self):
         tok = self.current_tok
 
-        if tok_type in  (TT_INT, TT_FLOAT):
+        if tok_type in (TT_INT, TT_FLOAT):
             self.adv()
             Return NumberNode(tok)
-    
-    def term(self):
-        Return self.bin_op(self.factor, (TT_MUl, TT_DIV))
-    
-    def expr(self):
-        Return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
 
-    def bin_op(self, func, ops):
-        lft = func()
+        def term(self):
+            Return self.bin_op(self.factor, (TT_MUL, TT_DIV))
 
-        while self.current_tok in ops:
-            op_tok = self.current_tok
-            self.adv()
-            rgt = func()
-            lft = BinOpNode(lft,op_tok, rgt)
+        def expr(self):
+            Return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
 
-        Return lft
+        def bin_op(self, func, ops):
+            lft = func()
 
-############################
-#Run
-############################
+            while self.current_tok in ops:
+                op_tok = self.current_tok
+                self.adv()
+                rgt = func()
+                lft = BinOpNode(lft,op_tok,rgt)
 
-def run(fn, text):
-    #this makes tokens
-    lexer = Lexer(fn, text)
-    tokens, error =  lexer.make_tokens()   
-    if error: Return None, error
+            Return lft
+#RUN
+################################
+        def run(self, fn, text):
+            lexer = Lexer(fn,text)
+            tokens, error =  lexer.make_tokens()
+            if error: Return None, error
 
-    #this makes the AST
-    parser = Parser(tokens)
-    ast = parser.parse()
+            #this is the AST
+            parser = Parser(tokens)
+            ast = parser.parse()
 
-    Return ast, None
+            Return ast, None
 
-delay_print('This a go again\n'
-'All you can put in is number and (+,*,/,-)'
-'dont mind two,\n' 'its a prototype\n')
+
+
+
+
