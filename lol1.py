94c7fce4cfc26165f81d8a58b79962d220eb093f
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-04-30 03:21:36 -0500

    Add files via upload

---
+++ lol1.py
@@ -0,0 +1,9 @@
+the Nstruct is gonna need the current_char and the starting pos
+
+def Current_Nstruct(self):
+    if current_char != '\n':
+        len(current_char,start_pos)
+    Return len #thinking this gon work cuz it local and not a global len func.
+
+
+
